57d4e28b6458ad31d7a444d0eebfb4df949529cb
ANA CLARA SILVESTRE DE OLIVEIRA <ana.oliveira33_estud@C110726.pdc-academico.ifms.tl>
2025-07-15 20:53:56 -0400

    ex4

---
+++ ex4.py
@@ -0,0 +1,21 @@
+def analisa_registros(registros: dict):
+    result = {}
+    for chave, valor in registros.items():
+        saldo_final = 0
+        for valor, tipo in valor:
+            if tipo == 'entrada':
+                saldo_final += valor
+            else:
+                saldo_final -= valor
+        result[chave] = saldo_final 
+    return result
+
+# Exemplo de uso:
+
+registros = {
+    "alimentacao": [(1, 'entrada'), (2, 'saida')],
+    "transporte": [(2, 'saida'), (3, 'saida')],
+    "lazer": [(4, 'entrada'), (5, 'entrada')]
+}
+
+print(analisa_registros(registros))
