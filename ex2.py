0c12def7bd6a6b5fe335df4c80f1da46ab609425
ANA CLARA SILVESTRE DE OLIVEIRA <ana.oliveira33_estud@C110726.pdc-academico.ifms.tl>
2025-07-15 21:41:15 -0400

    siplifica ex2

--- ex2.py
+++ ex2.py
@@ -2,21 +2,19 @@ def calcula_desconto(produtos: list, **opcoes):
     if produtos == []: return 'Nenhum produto cadastrado'
     
     desconto = opcoes.get('desconto', 0)/100
-    result = []
-    for p in produtos:
-        preco_atual = p.get('preco', 0)
-        nome = p.get('nome', '')
-        novo_preco = preco_atual - preco_atual * desconto if desconto != 0 else preco_atual
-        result.append((nome, novo_preco))
-    return result
+
+    return [
+        (p.get('nome', ''), p.get('preco', 0) - p.get('preco', 0) * desconto)
+        for p in produtos
+    ]
 
 # Exemplo de uso:
 
 lista_produtos = [
-    {"nome": "Batata", "preco": 10},
-    {"nome": "Maçã", "preco": 2},
-    {"nome": "Laranja", "preco": 5},
-    {"nome": "Uva", "preco": 12}
+    {"nome": "Batata", "preco": 100},
+    {"nome": "Maçã", "preco": 200},
+    {"nome": "Laranja", "preco": 50},
+    {"nome": "Uva", "preco": 120}
 ] 
 
-print(calcula_desconto(lista_produtos, desconto=10))
+print(calcula_desconto(lista_produtos))
